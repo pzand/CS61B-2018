2f658fe16d73dfe13ee3dd75a3672d56a569284d
‘pzand’ <pzand17875626301@163.com>
2022-09-17 13:22:17 +0800

    create an animation

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,18 +1,19 @@
 public class NBody {
-    public static Double readRadius(String fileName){
+    public static Double readRadius(String fileName) {
         In file = new In(fileName);
         file.readInt();
         return file.readDouble();
     }
-    public static Planet[] readPlanets(String fileName){
+
+    public static Planet[] readPlanets(String fileName) {
         In file = new In(fileName);
         int n = file.readInt();
-        double a = file.readDouble();
+        file.readDouble();
 
         Planet[] planets = new Planet[n];
-        for(int i = 0;i < n;i++){
+        for (int i = 0; i < n; i++) {
             planets[i] = new Planet(file.readDouble(), file.readDouble(), file.readDouble(),
-                           file.readDouble(), file.readDouble(), file.readString());
+                    file.readDouble(), file.readDouble(), file.readString());
         }
         return planets;
     }
@@ -22,15 +23,43 @@ public class NBody {
         double dt = Double.parseDouble(args[1]);
         String filename = args[2];
         double radius = readRadius(filename);
-
-        StdDraw.setScale(-radius, radius);
+        StdDraw.enableDoubleBuffering();        //enable double buffering
+        StdDraw.setScale(-radius, radius);      //set radius of universe
         StdDraw.clear();
 
-        StdDraw.picture(0, 0, "./images/starfield.jpg");
+        StdDraw.picture(0, 0, "./images/starfield.jpg");    //drawing the background
         Planet[] planets = readPlanets(filename);
 
-        for(Planet p : planets){
+        for (Planet p : planets) {                //Drawing All of the planets
             p.draw();
         }
+        StdDraw.show();
+
+        for (double nowtime = 0; nowtime < T; nowtime += dt) {
+            double[] xForce = new double[planets.length];
+            double[] yForce = new double[planets.length];
+            for (int i = 0; i < planets.length; i++) {
+                xForce[i] = planets[i].calcNetForceExertedByX(planets);
+                yForce[i] = planets[i].calcNetForceExertedByY(planets);
+            }
+
+            for (int i = 0; i < planets.length; i++) {
+                planets[i].update(dt, xForce[i], yForce[i]);
+            }
+
+            StdDraw.picture(0, 0, "./images/starfield.jpg");    //drawing the background
+            for (Planet p : planets) {                //Drawing All of the planets
+                p.draw();
+            }
+            StdDraw.show();
+            StdDraw.pause(1);
+        }
+//
+//        StdOut.printf("%d\n", planets.length);
+//        StdOut.printf("%.2e\n", radius);
+//        for (int i = 0; i < planets.length; i++) {
+//            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+//                    planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+//                    planets[i].yyVel, planets[i].mass, planets[i].imgFileName);
     }
 }
