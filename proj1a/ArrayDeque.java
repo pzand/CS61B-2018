79520cf0da33fc88c14cf0fc620400ae45ce82dc
‘pzand’ <pzand17875626301@163.com>
2022-10-25 20:40:34 +0800

    完成ArrayDeque和数组调整

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -6,44 +6,96 @@ public class ArrayDeque<T> implements Deque<T>{
      * @param item
      */
     private int size = 0;
-
+    private int nextLast = 1;
+    private int nextFirst = 0;
+    private double R = 0;
+    public T[] arr = (T[]) new Object[8];
     @Override
     public void addFirst(T item) {
-
+        this.size++;
+        arr[this.nextFirst] = item;
+        this.nextFirst = resizingToZeroOrLength(--this.nextFirst);
+        resizingArr();
     }
 
     @Override
     public void addLast(T item) {
-
+        this.size++;
+        arr[this.nextLast] = item;
+        this.nextLast = resizingToZeroOrLength(++this.nextLast);
+        resizingArr();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return (this.nextLast - this.nextFirst) == 1;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public void printDeque() {
-
+        for(int i = 1, num = this.nextFirst;i <= this.size;i++) {
+            num = resizingToZeroOrLength(num + 1);
+            System.out.print(arr[num] + " ");
+        }
     }
 
     @Override
     public T removeFirst() {
-        return null;
+        if(this.isEmpty()){
+            return null;
+        }
+        this.size--;
+        this.nextFirst = resizingToZeroOrLength(++this.nextFirst);
+        return this.arr[this.nextFirst];
     }
 
     @Override
     public T removeLast() {
-        return null;
+        if(this.isEmpty()){
+            return null;
+        }
+        this.size--;
+        this.nextLast = resizingToZeroOrLength(--this.nextLast);
+        return this.arr[this.nextLast];
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if(this.size < index) {
+            return null;
+        }else{
+            int new_index = resizingToZeroOrLength(this.nextFirst + index);
+            return arr[new_index];
+        }
+    }
+
+    public int resizingToZeroOrLength(int num){
+        return (num + arr.length) % arr.length;
+    }
+
+    private void resizingArr(){
+        this.R = (double)this.size / this.arr.length;
+        T[] arr2;
+
+        if(R >= 0.99){
+            arr2 = (T[]) new Object[this.arr.length * 2];
+        } else if (R <= 0.25 && arr.length > 8) {
+            arr2 = (T[]) new Object[this.arr.length / 2];
+        }else {
+            return;
+        }
+        int new_first = 0, old_first;
+        for(int i = 1;i <= this.size;i++){
+            new_first = (nextFirst + i + arr.length) % arr2.length;
+            old_first = (nextFirst + i) % arr.length;
+            arr2[new_first] = arr[old_first];
+        }
+        this.nextFirst = new_first + arr.length;
+        arr = arr2;
     }
 }
