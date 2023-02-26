d5617579dd688158fcde075869c51cb2af66802b
‘pzand’ <pzand17875626301@163.com>
2023-02-26 18:20:08 +0800

    修复sample

--- hw1/synthesizer/GuitarString.java
+++ hw1/synthesizer/GuitarString.java
@@ -60,6 +60,6 @@ public class GuitarString {
     /* Return the double at the front of the buffer. */
     public double sample() {
         // TODO: Return the correct thing.
-        return buffer.peek();
+        return buffer.peek() * 2;
     }
 }
