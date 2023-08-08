7bfb5d84f84e8418947c6c908538e88a474c4104
‘pzand’ <pzand17875626301@163.com>
2023-08-08 23:06:18 +0800

    完成可排序负数的计数排序，通过创建一个对应的负数数组实现

--- lab13/CountingSort.java
+++ lab13/CountingSort.java
@@ -2,7 +2,6 @@
  * Class with 2 ways of doing Counting sort, one naive way and one "better" way
  *
  * @author Akhil Batra, Alexander Hwang
- *
  **/
 public class CountingSort {
     /**
@@ -67,6 +66,55 @@ public class CountingSort {
      */
     public static int[] betterCountingSort(int[] arr) {
         // TODO make counting sort work with arrays containing negative numbers.
-        return null;
+        // 定义最小值 和 最大值，用于counting创建数组
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int num : arr) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        min = (min < 0 ? -min : -1);    // 数组中没有负数，则不创建负数数组
+        max += 1;
+        min += 1;
+        int[] positiveCounting = new int[max];
+        int[] negativeCounting = new int[min];
+
+        // 统计数字的个数
+        for (int num : arr) {
+            if (num >= 0) {
+                positiveCounting[num]++;
+            } else {
+                negativeCounting[-num]++;
+            }
+        }
+
+        // 确定每个数字排序后的位置
+        int[] positiveState = new int[max];
+        int[] negativeState = new int[min];
+        int pos = 0;    // 通过一个变量pos，保持优雅。
+        // positiveState[i] = positiveState[i - 1] + positiveCounting[i], ps[0] = c[0]。对于第一项需要判断
+        // 负数从后往前 正数从前往后
+        for (int i = negativeCounting.length - 1; i >= 0; i--) {
+            negativeState[i] = pos;
+            pos += negativeCounting[i];
+        }
+        for (int i = 0; i < positiveCounting.length; i++) {
+            positiveState[i] = pos;
+            pos += positiveCounting[i];
+        }
+
+        // 根据原数组 和 数字的实际位置，合成一个排序后的数组
+        int[] sorted = new int[arr.length];
+        for (int num : arr) {
+            if (num >= 0) {
+                sorted[positiveState[num]] = num;
+                positiveState[num]++;
+            } else {
+                sorted[negativeState[-num]] = num;
+                negativeState[-num]++;
+            }
+        }
+        return sorted;
     }
 }
